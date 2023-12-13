1c77d0ee0b03f7ebd1be3e862ca6ecd3aa6af8d0
nathalia_lima <Nathalialima926@gmail.com>
2023-12-12 23:29:34 -0300

    feature: novos endpoints da api

---
+++ src/index.js
@@ -0,0 +1,187 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const pool = require('./db');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+// Endpoint para login
+app.post('/login', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+
+        const result = await pool.query('SELECT * FROM usuarios WHERE username = $1 and password = $2', [username, password]);
+        
+        const user = result.rows[0]
+        
+        if(user){
+            console.log(user)
+            return res.status(200).json({ acesso: 'Autorizado' })
+        } else {
+            console.log(user)
+            return res.status(401).json({ acesso: 'Negado', error: 'Usuário ou senha incorretos' })
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Erro interno do servidor' });
+    }
+});
+
+// Endpoint para obter todos os usuários
+app.get('/usuarios', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM usuarios');
+        res.json(result.rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Erro interno do servidor' });
+    }
+});
+
+// Endpoint para adicionar um novo usuário
+app.post('/usuarios', async (req, res) => {
+    const { username, email, password } = req.body;
+
+    try {
+        const result = await pool.query('INSERT INTO usuarios (username, email, password) VALUES ($1, $2, $3) RETURNING *', [username, email, password]);
+        res.json(result.rows[0]);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Erro interno do servidor' });
+    }
+});
+
+// Endpoint para obter todos os jogos
+app.get('/jogos', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM jogos');
+        res.json(result.rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Erro interno do servidor' });
+    }
+});
+
+// Endpoint para adicionar um jogo
+app.post('/jogos', async (req, res) => {
+    const { nome, descricao } = req.body;
+
+    try {
+        const result = await pool.query('INSERT INTO jogos (nome, descricao) VALUES ($1, $2) RETURNING *', [nome, descricao]);
+        res.json(result.rows[0]);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Erro interno do servidor' });
+    }
+});
+
+// Endpoint para deletar um jogo
+app.delete('/jogos/:id', async (req, res) => {
+    const jogoId = req.params.id;
+
+    try {
+        const result = await pool.query('DELETE FROM jogos WHERE id = $1 RETURNING *', [jogoId]);
+
+        if (result.rows.length > 0) {
+            res.json({ message: 'Jogo removido com sucesso', removedGame: result.rows[0] });
+        } else {
+            res.status(404).json({ error: 'Jogo não encontrado' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Erro interno do servidor' });
+    }
+});
+
+// Endpoint para atualizar um jogo
+app.put('/jogos/:id', async (req, res) => {
+    const jogoId = req.params.id;
+    const { nome, descricao } = req.body;
+
+    try {
+        const result = await pool.query('UPDATE jogos SET nome = $1, descricao = $2 WHERE id = $3 RETURNING *', [nome, descricao, jogoId]);
+
+        if (result.rows.length > 0) {
+            res.json({ message: 'Jogo atualizado com sucesso', updatedGame: result.rows[0] });
+        } else {
+            res.status(404).json({ error: 'Jogo não encontrado' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Erro interno do servidor' });
+    }
+});
+
+// Endpoint para obter todas as plataformas de jogos
+app.get('/plataformas', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM plataforma_de_jogos');
+        res.json(result.rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Erro interno do servidor' });
+    }
+});
+
+// Endpoint para adicionar uma plataforma de jogos
+app.post('/plataformas', async (req, res) => {
+    const { nome } = req.body;
+
+    try {
+        const result = await pool.query('INSERT INTO plataforma_de_jogos (nome) VALUES ($1) RETURNING *', [nome]);
+        res.json(result.rows[0]);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Erro interno do servidor' });
+    }
+});
+
+// Endpoint para deletar uma plataforma de jogos
+app.delete('/plataformas/:id', async (req, res) => {
+    const plataformaId = req.params.id;
+
+    try {
+        const result = await pool.query('DELETE FROM plataforma_de_jogos WHERE id = $1 RETURNING *', [plataformaId]);
+
+        if (result.rows.length > 0) {
+            res.json({ message: 'Plataforma removida com sucesso', removedGame: result.rows[0] });
+        } else {
+            res.status(404).json({ error: 'Plataforma não encontrada' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Erro interno do servidor' });
+    }
+});
+
+// Endpoint para atualizar um jogo
+app.put('/plataformas/:id', async (req, res) => {
+    const plataformaId = req.params.id;
+    const { nome} = req.body;
+
+    try {
+        const result = await pool.query('UPDATE plataforma_de_jogos SET nome = $1 WHERE id = $2 RETURNING *', [nome, plataformaId]);
+
+        if (result.rows.length > 0) {
+            res.json({ message: 'Plataforma atualizada com sucesso', updatedGame: result.rows[0] });
+        } else {
+            res.status(404).json({ error: 'Plataforma não encontrada' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Erro interno do servidor' });
+    }
+});
+
+
+  
+  
+  
+
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
+
---
+++ src/migration.js
@@ -0,0 +1,114 @@
+const pool = require('./db');
+
+class PopularBanco {
+    async popular(){
+        this.criaTabelaDeJogos()
+        this.criaTabelaPlataformaDeJogos()
+        this.criaTabelaUsuarios
+    }
+    
+    async criaTabelaUsuarios(){
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS usuarios (
+                id SERIAL PRIMARY KEY,
+                username VARCHAR(50) UNIQUE NOT NULL,
+                password VARCHAR(100) NOT NULL,
+                email VARCHAR(100) UNIQUE NOT NULL
+            );
+        `)
+        console.log("Tabela usuarios criada com sucesso")
+        return
+    }
+    
+    async criaTabelaDeJogos(){
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS jogos (
+                id SERIAL PRIMARY KEY,
+                nome VARCHAR(60) UNIQUE NOT NULL,
+                descricao VARCHAR(150) NOT NULL
+            );
+        `)
+        console.log("Tabela jogos criada com sucesso")
+        return
+    }
+    
+    async criaTabelaPlataformaDeJogos(){
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS plataforma_de_jogos (
+                id SERIAL PRIMARY KEY,
+                nome VARCHAR(60) UNIQUE NOT NULL
+            );
+        `)
+        console.log("Tabela plataforma_de_jogos criada com sucesso")
+        return
+    }
+
+    async insereNovaPlataformaDeJogos(nome){
+        await pool.query(`
+            INSERT INTO plataforma_de_jogos (nome)
+            VALUES ($1)
+            ON CONFLICT (nome) DO NOTHING
+            RETURNING *;`, [nome]);
+    }
+    
+    async insereNovoUsuario(username, password, email ){
+        await pool.query(`
+            INSERT INTO usuarios (username, password, email)
+            VALUES ($1, $2, $3)
+            ON CONFLICT (username) DO NOTHING
+            RETURNING *;`, [username, password, email]);
+    }
+    
+    async insereNovoJogo(nome, descricao ){
+        await pool.query('INSERT INTO jogos (nome, descricao) VALUES ($1, $2) RETURNING *', [nome, descricao]);
+    }
+}
+
+const banco = new PopularBanco()
+
+ banco.popular()
+
+async function popularBanco(){
+
+// inserindo os 10 objetos de plataformas
+await banco.insereNovaPlataformaDeJogos("Playstation")
+await banco.insereNovaPlataformaDeJogos("Xbox")
+await banco.insereNovaPlataformaDeJogos("Steam")
+await banco.insereNovaPlataformaDeJogos("EpicGame")
+await banco.insereNovaPlataformaDeJogos("Android")
+await banco.insereNovaPlataformaDeJogos("Ios")
+await banco.insereNovaPlataformaDeJogos("Desktop")
+await banco.insereNovaPlataformaDeJogos("PSN")
+await banco.insereNovaPlataformaDeJogos("Streaming")
+await banco.insereNovaPlataformaDeJogos("Mobile")
+
+// inserindo os 10 objetos de usuarios
+await banco.insereNovoUsuario("usuario1", "senha1", "usuario1@email.com")
+await banco.insereNovoUsuario("usuario2", "senha2", "usuario2@email.com")
+await banco.insereNovoUsuario("usuario3", "senha3", "usuario3@email.com")
+await banco.insereNovoUsuario("usuario4", "senha4", "usuario4@email.com")
+await banco.insereNovoUsuario("usuario5", "senha5", "usuario5@email.com")
+await banco.insereNovoUsuario("usuario6", "senha6", "usuario6@email.com")
+await banco.insereNovoUsuario("usuario7", "senha7", "usuario7@email.com")
+await banco.insereNovoUsuario("usuario8", "senha8", "usuario8@email.com")
+await banco.insereNovoUsuario("usuario9", "senha9", "usuario9@email.com")
+await banco.insereNovoUsuario("usuario10", "senha10", "usuario10@email.com")
+
+// inserindo os 10 objetos de jogos
+await banco.insereNovoJogo("Jogo1", "Descrição 1")
+await banco.insereNovoJogo("Jogo2", "Descrição 2")
+await banco.insereNovoJogo("Jogo3", "Descrição 3")
+await banco.insereNovoJogo("Jogo4", "Descrição 4")
+await banco.insereNovoJogo("Jogo5", "Descrição 5")
+await banco.insereNovoJogo("Jogo6", "Descrição 6")
+await banco.insereNovoJogo("Jogo7", "Descrição 7")
+await banco.insereNovoJogo("Jogo8", "Descrição 8")
+await banco.insereNovoJogo("Jogo9", "Descrição 9")
+await banco.insereNovoJogo("Jogo10", "Descrição 10")
+}
+
+popularBanco()
+
+
+
+
