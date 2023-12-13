57b137fc2faf96fe524e01bfc0b6fef5071d1c41
nathalia_lima <Nathalialima926@gmail.com>
2023-12-13 15:48:26 -0300

    excluir jogo

--- frontend/game-library/src/components/CardJogos/CardJogos.js
+++ frontend/game-library/src/components/CardJogos/CardJogos.js
@@ -1,17 +1,33 @@
-import Button from 'react-bootstrap/Button';
+import React from 'react';
 import Card from 'react-bootstrap/Card';
-import jogosImg from '../../images/jogos.PNG'
+import Button from 'react-bootstrap/Button';
+import jogosImg from '../../images/jogos.PNG';
+import PropTypes from 'prop-types';
 
-function CardJogos({ title, text }) {
-    return (
-      <Card style={{ width: '18rem' }}  className="bg-dark mt-5">
-        <Card.Img variant="top" src={jogosImg} />
-        <Card.Body>
-          <Card.Title className="text-light">{title}</Card.Title>
-          <Card.Text className="text-light">{text}</Card.Text>
-        </Card.Body>
-      </Card>
-    );
-  }
+function CardJogos({ id, title, text, onDelete }) {
+  const handleDelete = () => {
+    onDelete(id);
+  };
+
+  return (
+    <Card style={{ width: '18rem' }} className="bg-dark mt-5">
+      <Card.Img variant="top" src={jogosImg} />
+      <Card.Body>
+        <Card.Title className="text-light">{title}</Card.Title>
+        <Card.Text className="text-light">{text}</Card.Text>
+        <Button variant="danger" onClick={handleDelete}>
+          Excluir
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+CardJogos.propTypes = {
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  onDelete: PropTypes.func.isRequired,
+};
 
 export default CardJogos;
--- frontend/game-library/src/components/ListaJogos/ListaJogos.js
+++ frontend/game-library/src/components/ListaJogos/ListaJogos.js
@@ -1,28 +1,44 @@
-import api from "../../services/api";
 import React, { useEffect, useState } from 'react';
-import CardJogos from '../CardJogos/CardJogos'
 import { Container, Row, Col } from 'react-bootstrap';
+import api from '../../services/api';
+import CardJogos from '../CardJogos/CardJogos';
 
 function ListaDeJogos() {
-    const [jogos, setJogos] = useState([]);
-  
-    useEffect(() => {
-      api.get('/jogos')
-        .then(response => setJogos(response.data))
-        .catch(error => console.error(error));
-    }, []);
-  
-    return (
-        <Container className="pb-5">
-        <Row>
-          {jogos.map(jogo => (
-            <Col key={jogo.id} sm={12} md={6} lg={4}>
-              <CardJogos title={jogo.nome} text={jogo.descricao} />
-            </Col>
-          ))}
-        </Row>
-      </Container>
-    );
-  }
-  
-  export default ListaDeJogos;
+  const [jogos, setJogos] = useState([]);
+
+  useEffect(() => {
+    api.get('/jogos')
+      .then(response => setJogos(response.data))
+      .catch(error => console.error(error));
+  }, []);
+
+  const handleDelete = (id) => {
+    // Faça uma chamada API para excluir o jogo com o ID fornecido
+    api.delete(`/jogos/${id}`)
+      .then(response => {
+        // Atualize o estado para refletir a exclusão
+        setJogos(prevJogos => prevJogos.filter(jogo => jogo.id !== id));
+      })
+      .catch(error => console.error(error));
+  };
+
+  return (
+    <Container className="pb-5">
+      <Row>
+        {jogos.map(jogo => (
+          <Col key={jogo.id} sm={12} md={6} lg={4}>
+            {/* Passe a função handleDelete e o ID do jogo como propriedades */}
+            <CardJogos
+              id={jogo.id}
+              title={jogo.nome}
+              text={jogo.descricao}
+              onDelete={handleDelete}
+            />
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+}
+
+export default ListaDeJogos;
